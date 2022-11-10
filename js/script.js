1d450bbc01dfc1f58a11a8f5db872a0f27a24e6e
Thomas-Longworth <thomaslongworth555@gmail.com>
2022-11-10 12:36:10 +0000

    made some functions in javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("working")
+
+const buttonA = document.getElementById("a-button")
+buttonA.addEventListener("click", aButtonClick)
+
+function runGame() {
+    console.log("hello")
+
+}
+function aButtonClick() {
+    console.log("working as intended")
+
+}
