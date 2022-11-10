5d68d47d9299c33584addcc6af2b7d2bbc3e71ff
Thomas-Longworth <thomaslongworth555@gmail.com>
2022-11-10 18:48:05 +0000

    added quiz questions

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ console.log("working")
 const buttonA = document.getElementById("a-button")
 buttonA.addEventListener("click", aButtonClick)
 
+const buttonB = document.getElementById("b-button")
+buttonB.addEventListener("click", bButtonClick)
+
+const buttonC = document.getElementById("c-button")
+buttonC.addEventListener("click", cButtonClick)
+
+const buttonD = document.getElementById("d-button")
+buttonD.addEventListener("click", dButtonClick)
+
+
+
+
 function runGame() {
     console.log("hello")
 
@@ -10,4 +22,17 @@ function runGame() {
 function aButtonClick() {
     console.log("working as intended")
 
-}
+} 
+
+function bButtonClick() {
+    console.log("working as intended")
+
+} 
+function cButtonClick() {
+    console.log("working as intended")
+
+} 
+function dButtonClick() {
+    console.log("working as intended")
+
+} 
